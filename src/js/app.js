285b5163f52fb327365b5b4998dbfa6c0981292d
Irina Khaustova <Irina170580@yandex.ru>
2022-03-10 22:12:56 +0300

    change app

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import Character from './character';
 import Team from './team';
 
-const character = new Character('Ivan', 'Swordsman');
+const character = new Character('Billi', 'Swordsman');
 const team = new Team();
 team.add(character);
